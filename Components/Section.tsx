194d630eb95518473257394549e2675b46ad0bc5
ayekaday <yukiakd279@gmail.com>
2024-12-05 14:01:51 +0630

    fix final

--- Components/Section.tsx
+++ Components/Section.tsx
@@ -1,31 +1,23 @@
 import { useMotionValueEvent, useScroll } from "framer-motion";
-import React, { ReactNode, useRef, useEffect } from "react";
+import React, { ReactNode, useRef } from "react";
 
 const Section: React.FC<{
   theme: string;
   setTheme: (value: string) => void;
   children: ReactNode;
 }> = ({ theme, setTheme, children }) => {
-  const container = useRef<HTMLElement>(null);
+  const container = useRef<HTMLElement>(null); // Explicit type for container
 
   const { scrollYProgress } = useScroll({
     target: container,
     offset: ["start center", "end center"],
   });
 
-  useMotionValueEvent(scrollYProgress, "change", (value: number) => {
-    // Switch theme only if it differs from the current one
-    if (value > 0 && value < 1 && theme !== "dark") {
-      setTheme("dark");
-    } else if (value <= 0 && theme !== "light") {
-      setTheme("light");
+  useMotionValueEvent(scrollYProgress, "change", (value) => {
+    if (value > 0 && value < 1) {
+      setTheme(theme);
     }
   });
-
-  useEffect(() => {
-    // Set the initial theme when the component mounts
-    document.documentElement.className = theme;
-  }, [theme]);
 
   return (
     <section
